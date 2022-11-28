c3803afa378d4cc4a86e5ced8de2c8dfd27fca15
emma-rae03 <111804721+emma-rae03@users.noreply.github.com>
2022-11-28 16:30:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("h1") .addEventListener("click", function (){
-  var colorRandom = Math.floor(Math.random()*16777215).tostring(16); 
+  var colorRandom = Math.floor(Math.random()*16777215).toString(16); 
   document.body.style.color ="#"+colorRandom; 
 }); 
